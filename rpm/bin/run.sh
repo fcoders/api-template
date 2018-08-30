53d215fe88a51b00929955db91b83348b15510a9
robert <robert@astropay.com>
2018-08-30 15:37:31 -0300

    Fixed log

--- rpm/bin/run.sh
+++ rpm/bin/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd /opt/${project.artifactId}
-su -s /bin/sh ${rpm.username} -c "/opt/${project.artifactId}/${project.artifactId}" >>/var/log/${project.artifactId}/access.log 2>>/var/log/${project.artifactId}/server.log &
+su -s /bin/sh ${rpm.username} -c "/opt/${project.artifactId}/${project.artifactId}" >>/var/log/${project.artifactId}/serfver.log 2>>/var/log/${project.artifactId}/server.log &
 echo $!>/var/run/${project.artifactId}.pid
